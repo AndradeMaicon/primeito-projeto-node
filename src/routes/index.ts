14a92dd7052d776ab7aee58bd6a9e10cc5922506
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-05-24 17:10:43 -0300

    isntall prettier

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hello Maicon' }));
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello Maicon' }),
+);
 
 export default routes;
